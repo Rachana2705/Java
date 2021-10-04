544d521ba598c096779880b60bcf4a1baf97a85a
rachana2705 <rachana27.xworkz@gmail.com>
2021-10-04 09:48:21 +0530

    New programs added

---
+++ core/Ornament.java
@@ -0,0 +1,5 @@
+class Ornament{
+public static String Ornament_type(String[] Ornament){
+return Ornament[0];
+}
+}
---
+++ core/OrnamentRunner.java
@@ -0,0 +1,7 @@
+class OrnamentRunner{
+public static void main(String[] args){
+String Orna = {"necklace","earrings","bangles","nosering","rings"};
+String res = Ornament.Ornament_type(String[] Orna);
+System.out.println(res);
+}
+}
