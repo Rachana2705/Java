8c9c71a358c350a2e8ef0f088407ab10c8ed6632
rachana2705 <rachana27.xworkz@gmail.com>
2021-10-11 22:58:05 +0530

    initialize using 'this' keyword

--- core/Stabilizer.java
+++ core/Stabilizer.java
@@ -12,7 +12,7 @@ String appliances;
 String purchaseMode;
 float powerFactor;
 int warranty;
-float size = "medium";
+float size;
 float efficiency;
 float weight;
 
@@ -21,12 +21,14 @@ Stabilizer(){
 }
 
 Stabilizer(float cost){
+	System.out.println("Invoked cost constructor");
     System.out.println(cost);
 	price=cost;
 	this.price = cost;
 }
 
 Stabilizer(String colour,int voltage,float powerRating){
+	System.out.println("Invoked colour,voltage and powerRating constructor");
 	 this.colour=colour;
 	 this.voltage=voltage;
 	 this.powerRating=powerRating;
@@ -34,6 +36,7 @@ Stabilizer(String colour,int voltage,float powerRating){
 }
 
 Stabilizer(int supplyVolt,int amperes,float efficiency,float weight,float powerFactor){
+	System.out.println("Invoked supplyVolt, amperes, efficiency, weight and powerFactor constructor");
 	 this.supplyVolt=supplyVolt;
 	 this.amperes=amperes;
 	 this.efficiency=efficiency;
@@ -42,12 +45,14 @@ Stabilizer(int supplyVolt,int amperes,float efficiency,float weight,float powerF
 }
 
 Stabilizer(String appliances,String purchaseMode,int warranty){
+	System.out.println("Invoked appliances, purchaseMode and warranty constructor");
 	 this.appliances=appliances;
 	 this.purchaseMode=purchaseMode;
 	 this.warranty=warranty;
 }
 
 Stabilizer(float size,String brand,float price){
+	System.out.println("Invoked size,brand and price constructor");
 	System.out.println(this.size);
 	System.out.println(this.brand);
 	System.out.println(this.price);
@@ -57,6 +62,7 @@ Stabilizer(float size,String brand,float price){
 }
 
 Stabilizer(float ratings,float powerFactor){
+	System.out.println("Invoked ratings, powerFactor constructor");
 	this.ratings = ratings;
 	this.powerFactor = powerFactor;
 }
